fc8cec7b199f8219cd9313ad9e16ae52ecec94f4
Roosevelt Blow <rooseveltblow@gmail.com>
2022-12-16 11:18:13 -0700

    adds form to New view

--- Views/New.jsx
+++ Views/New.jsx
@@ -6,6 +6,19 @@ class New extends React.Component{
         return (
             <DefaultLayout>
                 <h1>New</h1>
+                <form action="/logs" method="POST">
+                    <label htmlFor="title">Title:</label>
+                    <input type="text" name="title" id="title" />
+                    <br />
+                    <label htmlFor="entry">Log: </label>
+                    <textarea name="entry" id="entry" cols="30" rows="10"></textarea>
+                    <br />
+                    <label htmlFor="shipIsBroken">Ship Is Broken</label>
+                    <input type="checkbox" name="shipIsBroken" id="shipIsBroken" />
+                    <br />
+                    <input type="submit" value="Post Log" />
+                </form>
+                <a href="/logs">Back</a>
             </DefaultLayout>
         )
     }
